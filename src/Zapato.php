424f0c5fc2cdd70eb5622c41e04c98bf12db7e57
mirllan <edilsonmirllan038@gmail.com>
2024-10-30 16:11:15 -0500

    Initial commit

---
+++ src/Zapato.php
@@ -0,0 +1,5 @@
+<?php
+namespace varishop;
+class Zapatos {
+    
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'varishop/tienda-online',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'varishop/tienda-online' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
